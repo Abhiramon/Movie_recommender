c0e33fb86daa9a0844ece8f1c534d10b1fb30bae
Abhiramon <Abhiramon.R@iiitb.org>
2019-03-26 16:57:18 +0530

    basic website built

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, url_for, request
+from recommender_engine import get_recommendations
+
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template("home.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/result', methods = ["GET", "POST"])
+def result():
+	if request.method == "POST":
+		movie_name = request.form["movie_name"]
+
+	else:
+		movie_name = request.args.get("movie_name")
+
+	recommended_movies = get_recommendations(movie_name)
+
+	return render_template("result.html", given_movie = movie_name, movies = recommended_movies)
+
+
+if __name__ == "__main__":
+	app.run(debug = True)
---
+++ recommender_engine.py
@@ -0,0 +1,2 @@
+def get_recommendations(name):
+	return ["shawshank redemption", "how i met your mother", "Schindler's list"]
