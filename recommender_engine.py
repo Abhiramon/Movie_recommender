dc698c2d097e638972f5f3f30dd1e2cbbc157e44
Aadhithya Ramesh <aadhithya.ramesh@gmail.com>
2019-04-21 14:32:30 +0530

    Removed redundant libraries

--- recommender_engine.py
+++ recommender_engine.py
@@ -19,15 +19,11 @@
 import time
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy import stats
 from ast import literal_eval
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
-from nltk.stem.snowball import SnowballStemmer
-from nltk.stem.wordnet import WordNetLemmatizer
-from nltk.corpus import wordnet
-from surprise import Reader, Dataset, SVD, evaluate
+
+# from surprise import Reader, Dataset, SVD, evaluate
 
 import warnings; warnings.simplefilter('ignore')
 
